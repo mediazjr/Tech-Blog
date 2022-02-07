7fb8d45e8af7be2b05dc1fa8f3c2621ae0b902f4
mediazjr <mediazjr@gmail.com>
2022-02-07 10:58:52 -0800

    updated API routes and index.js in controllers, added new comment js in public, added edit post handlebars and fixed routing in views

--- controllers/api/postRoutes.js
+++ controllers/api/postRoutes.js
@@ -1,35 +1,52 @@
 const router = require('express').Router();
-const { Project } = require('../../models');
+const { Post } = require('../../models');
 const withAuth = require('../../utils/auth');
 
 router.post('/', withAuth, async (req, res) => {
   try {
-    const newProject = await Project.create({
+    const newPost = await Post.create({
       ...req.body,
       user_id: req.session.user_id,
     });
 
-    res.status(200).json(newProject);
+    res.status(200).json(newPost);
   } catch (err) {
     res.status(400).json(err);
   }
 });
 
+router.put('/:id', withAuth, async(req, res) => {
+  try {
+      console.log('hello')
+      const newPost = await Post.update(req.body, {
+          where: {
+              user_id: req.session.user_id,
+              id: req.params.id
+          }
+      });
+
+      res.status(200).json(newPost);
+  } catch (err) {
+      console.log(err)
+      res.status(400).json(err);
+  }
+});
+
 router.delete('/:id', withAuth, async (req, res) => {
   try {
-    const projectData = await Project.destroy({
+    const postData = await Post.destroy({
       where: {
         id: req.params.id,
         user_id: req.session.user_id,
       },
     });
 
-    if (!projectData) {
+    if (!postData) {
       res.status(404).json({ message: 'No project found with this id!' });
       return;
     }
 
-    res.status(200).json(projectData);
+    res.status(200).json(postData);
   } catch (err) {
     res.status(500).json(err);
   }
--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,11 +1,11 @@
 const router = require('express').Router();
-const { Project, User } = require('../models');
+const { Comment, Post, User} = require('../models');
 const withAuth = require('../utils/auth');
 
 router.get('/', async (req, res) => {
   try {
     // Get all projects and JOIN with user data
-    const projectData = await Project.findAll({
+    const postData = await Post.findAll({
       include: [
         {
           model: User,
@@ -15,7 +15,7 @@ router.get('/', async (req, res) => {
     });
 
     // Serialize data so the template can read it
-    const projects = projectData.map((project) => project.get({ plain: true }));
+    const projects = postData.map((post) => post.get({ plain: true }));
 
     // Pass serialized data and session flag into template
     res.render('homepage', { 
@@ -29,7 +29,7 @@ router.get('/', async (req, res) => {
 
 router.get('/project/:id', async (req, res) => {
   try {
-    const projectData = await Project.findByPk(req.params.id, {
+    const postData = await Post.findByPk(req.params.id, {
       include: [
         {
           model: User,
@@ -38,14 +38,41 @@ router.get('/project/:id', async (req, res) => {
       ],
     });
 
-    const project = projectData.get({ plain: true });
+    const post = postData.get({ plain: true });
 
-    res.render('project', {
-      ...project,
+    res.render('post', {
+      ...post,
       logged_in: req.session.logged_in
     });
   } catch (err) {
     res.status(500).json(err);
+  }
+});
+
+router.get('/editpost/:id', async(req, res) => {
+  try {
+      const postData = await Post.findByPk(req.params.id, {
+          include: [{
+              model: User,
+              attributes: ['name'],
+          }, {
+              model: Comment,
+              attributes: ['content'],
+              include: [{
+                  model: User,
+                  attributes: ['name'],
+              }]
+          }],
+      });
+
+      const post = postData.get({ plain: true });
+      console.log(post)
+      res.render('singlepost', {
+          ...post,
+          logged_in: req.session.logged_in
+      });
+  } catch (err) {
+      res.status(500).json(err);
   }
 });
 
